557bfb34bfecd4c5bb2978407ee5620d27928a6f
Gdawson771 <gdawson771@gmail.com>
2024-05-21 21:06:10 +0100

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="blue-gradient h-screen">
+    <Navbar />
+    <div class="h-96 items-center text-white flex justify-center w-full text-4xl">Hi, I'm Sophie</div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '~/components/Navbar.vue';
+</script>
+
+<style scoped>
+.blue-gradient {
+  background-image: linear-gradient(0deg,
+    hsl(221deg 91% 9%) 0%,
+    hsl(221deg 89% 10%) 9%,
+    hsl(219deg 86% 12%) 18%,
+    hsl(218deg 85% 13%) 27%,
+    hsl(216deg 83% 14%) 36%,
+    hsl(216deg 80% 16%) 45%,
+    hsl(214deg 79% 17%) 55%,
+    hsl(213deg 76% 18%) 64%,
+    hsl(211deg 74% 19%) 73%,
+    hsl(210deg 72% 21%) 82%,
+    hsl(209deg 70% 22%) 91%,
+    hsl(207deg 68% 23%) 100%);
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="w-full">
+                <ul class="flex w-full justify-around">
+                    <li>
+                        <NuxtLink to="/about-me" class="text-white hover:text-gray-300">About Me</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/publications" class="text-white hover:text-gray-300">Publications</NuxtLink>
+                    </li>
+                    <li>
+                        symbol
+                    </li>
+                    <li>
+                        <NuxtLink to="/courses" class="text-white hover:text-gray-300">Courses</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/tutoring" class="text-white hover:text-gray-300">Tutoring</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Add any scoped styles for the Navbar component here */
+
+</style>
